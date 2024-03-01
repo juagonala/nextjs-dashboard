ad7808280389d61d3f453a1ded4fdf5f21102e0a
Juan José González Alanís <juanjo.goal@gmail.com>
2024-03-01 16:45:12 +0100

    Removed typo from the Invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-s
+
 export default async function Page({
     searchParams,
   }: {
